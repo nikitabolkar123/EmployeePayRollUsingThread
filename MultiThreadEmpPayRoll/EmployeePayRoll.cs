3208e3482e06efc9ebaa9d9f23b580838285ce73
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-12-17 09:16:24 -0800

    Added UC3-RecordAddedPersonStartAndStopTime

--- MultiThreadEmpPayRoll/EmployeePayRoll.cs
+++ MultiThreadEmpPayRoll/EmployeePayRoll.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,19 +9,21 @@ namespace MultiThreadEmpPayRoll
     public class EmployeePayRoll
     {
         public List<EmpData> employeePolyeeDetailList = new List<EmpData>();
-
-        //UC 1
+        //UC 1 & 3
         public void addEmployeeToPayroll(List<EmpData> employeePayrollDataList)
         {
             employeePayrollDataList.ForEach(employeeData =>
             {
+                Stopwatch Time = new Stopwatch();
+                Time.Start();
                 Console.WriteLine("Employee Being Added :" + employeeData.EmployeeName);
                 this.addEmployeeToPayroll(employeeData);
-                Console.WriteLine("Employee added : " + employeeData.EmployeeName);
+                Time.Stop();
+                Console.WriteLine("Employee added : " + employeeData.EmployeeName + " ( Duration  : " + Time.Elapsed + ")");
             });
             Console.WriteLine(this.employeePolyeeDetailList.ToString());
         }
-        //UC 2
+        //UC 2 & 3
         public void addEmployeeToPayrollWithThread(List<EmpData> employeePayrollDataList)
         {
             employeePayrollDataList.ForEach(employeeData =>
@@ -29,10 +32,13 @@ namespace MultiThreadEmpPayRoll
                 {
                     //For Every iterarion of foreach loop, every record will get in to thread, every thread will be
                     //running in a single process to make execution quick and fast.
+                    Stopwatch Time = new Stopwatch();
+                    Time.Start();
 
                     Console.WriteLine("Employee Being Added :" + employeeData.EmployeeName);
                     this.addEmployeeToPayroll(employeeData);
-                    Console.WriteLine("Employee added : " + employeeData.EmployeeName);
+                    Time.Stop();
+                    Console.WriteLine("Employee added : " + employeeData.EmployeeName + " ( Duration : " + Time.Elapsed + ")");
                 });
                 thread.Start();
             });
